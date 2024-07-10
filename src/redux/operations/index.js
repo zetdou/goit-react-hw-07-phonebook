09e36d9d46f656dab4eed9401030307c57542a21
zetdou <zyziupoz@gmail.com>
2024-07-10 20:10:03 +0200

    Conversion async to createAsyncThunk
    Without adding same name function

--- src/redux/operations/index.js
+++ src/redux/operations/index.js
@@ -1,60 +1,22 @@
 import axios from "axios";
-import { fetchingError, fetchingInProgress, fetchingSuccess, addContactSuccess, removeContactSuccess } from "../slices/contactsSlice";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
 const apiClient = axios.create({
     baseURL: "https://668c1f1c0b61b8d23b0c7a63.mockapi.io/api/v1",
 });
 
-// wykorzystanie domniemanego zwrotu z funkcjami strzałkowymi
+export const fetchContacts = createAsyncThunk("contacts/fetchAll", async () => {
+    const { data } = await apiClient.get("/contacts");
+    return data;
+});
 
+export const addContact = createAsyncThunk("contacts/addContact", async (contact) => {
+    const { data } = await apiClient.post("/contacts", contact);
+    return data;
+});
 
-// const fetchContacts = () => async (dispatch) => {
-//     try {
-//         dispatch(fetchingInProgress());
-//         const response = await apiClient.get("/contacts")
-//         dispatch(fetchingSuccess(response.data));
-//     } catch (err) {
-//         dispatch(fetchingError(err.message));
-//     }
-// }
-
-// mechanika jak powyzej ale z wykorzystaniem dosłownych funkcji
-
-
-export function fetchContacts() {
-    return async function (dispatch) {
-        try {
-            dispatch(fetchingInProgress());
-            const response = await apiClient.get("/contacts");
-            dispatch(fetchingSuccess(response.data));
-        }   catch (err) {
-            dispatch(fetchingError(err.message));
-        }
-    };
-};
-
-export function addContact(contact) {
-    return async function (dispatch) {
-        try {
-            dispatch(fetchingInProgress());
-            const response = await apiClient.post("/contacts", contact);
-            dispatch(addContactSuccess(response.data));
-        }   catch (err) {
-            dispatch(fetchingError(err.message));
-        }
-    };
-}
-
-export function removeContact(id) {
-    return async function (dispatch) {
-        try {
-            dispatch(fetchingInProgress());
-            const response = await apiClient.delete(`/contacts/${id}`);
-            dispatch(removeContactSuccess(id));
-        }   catch (err) {
-            dispatch(fetchingError(err.message));
-        }
-    };
-}
+export const removeContact = createAsyncThunk("contacts/removeContact", async (id) => {
+    await apiClient.delete(`/contacts/${id}`);
+    return id;
+});
 
--- src/redux/slices/contactsSlice.js
+++ src/redux/slices/contactsSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchContacts, addContact, removeContact } from "../operations";
 
 const contactsSlice = createSlice({
     name: "contacts",
@@ -7,27 +8,45 @@ const contactsSlice = createSlice({
         loading: false,
         error: null,
     },
-    reducers: {
-        fetchingInProgress(state) {
+    extraReducers: (builder) => {
+        builder
+        .addCase(fetchContacts.pending, (state, action) => {
             state.loading = true;
-        },
-        fetchingSuccess(state, action) {
+        })
+        .addCase(fetchContacts.fulfilled, (state, action) => {
             state.loading = false;
             state.error = null;
             state.items = action.payload;
-        },
-        fetchingError(state, action) {
+        })
+        .addCase(fetchContacts.rejected, (state, action) => {
             state.loading = false;
             state.error = action.payload;
-        },
-        addContactSuccess(state, action) {
+        })
+        .addCase(addContact.pending, (state, action) => {
+            state.loading = true;
+        })
+        .addCase(addContact.fulfilled, (state, action) => {
+            state.loading = false;
+            state.error = null;
             state.items.push(action.payload);
-        },
-        removeContactSuccess(state, action) {
+        })
+        .addCase(addContact.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+        })
+        .addCase(removeContact.pending, (state, action) => {
+            state.loading = true;
+        })
+        .addCase(removeContact.fulfilled, (state, action) => {
+            state.loading = false;
+            state.error = null;
             state.items = state.items.filter(contact => contact.id !== action.payload);
-        },
-    },
+        })
+        .addCase(removeContact.rejected, (action, payload) => {
+            state.loading = false;
+            state.error = action.payload;
+        })
+    }
 });
 
 export default contactsSlice.reducer;
-export const { fetchingInProgress, fetchingSuccess, fetchingError, addContactSuccess, removeContactSuccess } = contactsSlice.actions;
